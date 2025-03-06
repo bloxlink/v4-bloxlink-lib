845c236c09014644c993ef5c50a43d3d40560446
Julien Kmec <me@julien.dev>
2025-03-06 11:44:49 -0800

    Switch to Pydantic settings (#28)
    * Switch to Pydantic settings
    
    * chore: sync requirements.txt with poetry.lock
    
    * remove dotenv
    
    * mark these as optional
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>

--- bloxlink_lib/config.py
+++ bloxlink_lib/config.py
@@ -1,41 +1,39 @@
-from os import getcwd, environ
 from typing import Literal
-from dotenv import load_dotenv
-from .models import BaseModel
-
-load_dotenv(f"{getcwd()}/.env")
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Config(BaseModel):
+class Config(BaseSettings):
     """Type definition for config values."""
 
-    DISCORD_TOKEN: str = None
+    DISCORD_TOKEN: str | None = None
     BOT_RELEASE: Literal["LOCAL", "CANARY", "MAIN", "PRO"]
     #############################
     # these are optional because we can choose to use MONGO_URL or MONGO_HOST/MONGO_USER/MONGO_PASSWORD/MONGO_PORT
-    MONGO_URL: str = None
-    MONGO_HOST: str = None
-    MONGO_PORT: int = 27017
-    MONGO_USER: str = None
-    MONGO_PASSWORD: str = None
-    MONGO_CA_FILE: str = None
+    MONGO_URL: str | None = None
+    MONGO_HOST: str | None = None
+    MONGO_PORT: int | None = 27017
+    MONGO_USER: str | None = None
+    MONGO_PASSWORD: str | None = None
+    MONGO_CA_FILE: str | None = None
     # these are optional because we can choose to use REDIS_URL or REDIS_HOST/REDIS_PORT/REDIS_PASSWORD
-    REDIS_URL: str = None
-    REDIS_HOST: str = None
-    REDIS_PORT: str = "6379"
-    REDIS_PASSWORD: str = None
+    REDIS_URL: str | None = None
+    REDIS_HOST: str | None = None
+    REDIS_PORT: str | None = "6379"
+    REDIS_PASSWORD: str | None = None
     #############################
-    PROXY_URL: str = None
-    DISCORD_PROXY_URL: str = None
-    BOT_API: str = None
-    BOT_API_AUTH: str = None
-    SENTRY_DSN: str = None
+    PROXY_URL: str | None = None
+    DISCORD_PROXY_URL: str | None = None
+    BOT_API: str | None = None
+    BOT_API_AUTH: str | None = None
+    SENTRY_DSN: str | None = None
     #############################
     LOG_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR"] = "INFO"
     SHARD_COUNT: int = 1
     SHARDS_PER_NODE: int = 1
     #############################
     TEST_MODE: bool = False  # if true, skip database and redis connections
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     def model_post_init(self, __context):
         # easier to validate with python expressions instead of attrs validators
@@ -76,6 +74,4 @@ class Config(BaseModel):
             )
 
 
-CONFIG: Config = Config(
-    **{field: value for field, value in environ.items() if field in Config.model_fields}
-)
+CONFIG: Config = Config()
