4b66913193706e1437f25e77bb31a9033e172dd0
Julien Kmec <me@julien.dev>
2025-04-23 11:03:09 -0700

    switch to http.HTTPStatus

--- bloxlink_lib/exceptions.py
+++ bloxlink_lib/exceptions.py
@@ -1,3 +1,6 @@
+from http import HTTPStatus
+
+
 class BloxlinkException(Exception):
     """Base exception for Bloxlink."""
 
@@ -6,7 +9,7 @@ class BloxlinkException(Exception):
         message: str | None = None,
         *,
         send_ephemeral: bool = False,
-        status_code: int = 400
+        status_code: int = HTTPStatus.BAD_REQUEST,
     ):
         self.message = message
         self.send_ephemeral = send_ephemeral  # Used exclusively by the bot to send the message as an ephemeral message. Not shown in web responses.
@@ -18,27 +21,41 @@ class BloxlinkException(Exception):
 class Message(BloxlinkException):
     """Generic exception to communicate some message to the user. Not necessarily an error."""
 
+    status_code = HTTPStatus.OK
+
 
 # ERRORS
 class Error(BloxlinkException):
     """Generic error."""
 
+    status_code = HTTPStatus.INTERNAL_SERVER_ERROR
+
 
 class RobloxNotFound(Error):
     """Raised when a Roblox entity is not found."""
+
+    status_code = HTTPStatus.NOT_FOUND
 
 
 class RobloxAPIError(Error):
     """Raised when the Roblox API returns an error."""
 
+    status_code = HTTPStatus.BAD_REQUEST
+
 
 class RobloxDown(Error):
     """Raised when the Roblox API is down."""
+
+    status_code = HTTPStatus.SERVICE_UNAVAILABLE
 
 
 class UserNotVerified(Error):
     """Raised when a user is not verified."""
 
+    status_code = HTTPStatus.FORBIDDEN
+
 
 class BloxlinkForbidden(Error):
     """Raised when a user lacks permissions."""
+
+    status_code = HTTPStatus.FORBIDDEN
