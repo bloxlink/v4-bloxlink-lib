916a89662196eb6cae949cbb5d957f76f6e2d895
Julien Kmec <me@julien.dev>
2025-05-01 22:45:43 -0400

    mark with @pytest.mark.asyncio

--- tests/integration/test_database_utilities.py
+++ tests/integration/test_database_utilities.py
@@ -10,6 +10,7 @@ class TestUpdatingGuildData:
     """Tests for converting V3 whole group binds to V4."""
 
     @pytest.mark.parametrize("test_input", ["sadasda", "Very awesome role"])
+    @pytest.mark.asyncio
     async def test_update_guild_data(
         self, test_input, start_docker_services, wait_for_redis
     ):
@@ -20,6 +21,7 @@ class TestUpdatingGuildData:
         ).verifiedRoleName == test_input
 
     @pytest.mark.parametrize("test_input", [5, 0, -1])
+    @pytest.mark.asyncio
     async def test_update_guild_data_invalid_input_fails(
         self, test_input, start_docker_services, wait_for_redis
     ):
