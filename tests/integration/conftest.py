b56747aa87956458e0baef4be5a4eba1fa912362
Julien Kmec <me@julien.dev>
2025-05-01 23:17:59 -0400

    mark fixtures with pytest_asyncio

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,11 +1,11 @@
 import asyncio
-import pytest
+import pytest_asyncio
 from bloxlink_lib.database.redis import (  # pylint: disable=no-name-in-module
     wait_for_redis as wait_for_redis_,
 )
 
 
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session")
 def event_loop(request):
     """Create an instance of the default event loop for each test case."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
@@ -13,12 +13,12 @@ def event_loop(request):
     loop.close()
 
 
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session")
 def start_docker_services(docker_services):
     """Start the Docker services."""
 
 
-@pytest.fixture(scope="function")
+@pytest_asyncio.fixture(scope="function")
 async def wait_for_redis():
     """Wait for Redis to be ready."""
 
