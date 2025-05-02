e0d0f24a245733cb344e3d40de4d3bfadf976817
Julien Kmec <me@julien.dev>
2025-05-01 23:00:24 -0400

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,12 @@ from bloxlink_lib.database.redis import (  # pylint: disable=no-name-in-module
 )
 
 
-# @pytest.fixture(scope="session")
-# def event_loop(request):
-#     """Create an instance of the default event loop for each test case."""
-#     loop = asyncio.get_event_loop_policy().new_event_loop()
-#     yield loop
-#     loop.close()
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(scope="session")
