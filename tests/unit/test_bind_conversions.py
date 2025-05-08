346831c4f86e92e77b5c417a90f1e82f7564d140
Julien Kmec <me@julien.dev>
2025-05-07 18:34:04 -0700

    Remove duplicate test case (#39)

--- tests/unit/test_bind_conversions.py
+++ tests/unit/test_bind_conversions.py
@@ -23,28 +23,10 @@ class TestBindConversions:
             correct_v4_binds
         ), f"Converted binds should have {len(correct_v4_binds)} binds."
 
-    def test_bind_equals_same_bind(
+    def test_correct_conversion(
         self, bind_conversion_test_data: BindConversionTestCase
     ):  # pylint: disable=W0621
-        """Test that the converted binds equal the same binds."""
-
-        v3_binds = bind_conversion_test_data.v3_binds
-        correct_v4_binds = bind_conversion_test_data.v4_binds
-
-        converted_binds = binds.GuildBind.from_V3(v3_binds)
-
-        assert all(
-            converted_bind == correct_bind
-            for converted_bind, correct_bind in zip(converted_binds, correct_v4_binds)
-        )
-
-    def test_bind_equals_another(
-        self, bind_conversion_test_data: BindConversionTestCase
-    ):  # pylint: disable=W0621
-        """Test that the converted binds equal the same binds."""
-
-        print(bind_conversion_test_data.v4_binds)
-        print(binds.GuildBind.from_V3(bind_conversion_test_data.v3_binds))
+        """Test that binds converted correctly."""
 
         assert bind_conversion_test_data.v4_binds == binds.GuildBind.from_V3(
             bind_conversion_test_data.v3_binds
