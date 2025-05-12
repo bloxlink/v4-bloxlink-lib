c8485899453c285e6df98d705e6fcc2fbe89a406
Julien Kmec <me@julien.dev>
2025-05-11 17:31:26 -0700

    wip

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,25 +1,48 @@
 import asyncio
+from typing import Final
+
 import pytest_asyncio
 from bloxlink_lib.database.redis import (  # pylint: disable=no-name-in-module
     wait_for_redis as wait_for_redis_,
 )
+from bloxlink_lib.database.mongodb import mongo
+
+TEST_GUILD_ID: Final[int] = 123
 
 
 @pytest_asyncio.fixture(scope="session")
-def event_loop(request):
+def event_loop():
     """Create an instance of the default event loop for each test case."""
+
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
+
     loop.close()
 
 
-@pytest_asyncio.fixture(scope="session")
-def start_docker_services(docker_services):
+@pytest_asyncio.fixture(scope="session", autouse=True)
+def start_docker_services(docker_services):  # pylint: disable=unused-argument
     """Start the Docker services."""
 
 
-@pytest_asyncio.fixture(scope="function")
+@pytest_asyncio.fixture(scope="function", autouse=True)
 async def wait_for_redis():
     """Wait for Redis to be ready."""
 
     await wait_for_redis_()
+
+
+@pytest_asyncio.fixture(scope="function")
+async def setup_clean_guild_data(
+    docker_services, wait_for_redis
+):  # pylint: disable=unused-argument
+    """Starts the database fresh for each test."""
+
+    await mongo.db.guilds.delete_one({"_id": TEST_GUILD_ID})
+
+
+@pytest_asyncio.fixture(scope="session")
+async def test_guild_id():
+    """Returns the test guild ID."""
+
+    return TEST_GUILD_ID
--- tests/integration/test_database_utilities.py
+++ tests/integration/test_database_utilities.py
@@ -6,26 +6,24 @@ from bloxlink_lib.models.schemas.guilds import (  # pylint: disable=no-name-in-m
 from pydantic import ValidationError
 
 
-class TestUpdatingGuildData:
+class TestIntegrationDatabaseUtilities:
     """Tests for converting V3 whole group binds to V4."""
 
     @pytest.mark.parametrize("test_input", ["sadasda", "Very awesome role"])
     @pytest.mark.asyncio
-    async def test_update_guild_data(
-        self, test_input, start_docker_services, wait_for_redis
-    ):
-        await update_guild_data(1, verifiedRoleName=test_input)
+    async def test_update_guild_data(self, test_input: str, test_guild_id: int):
+        await update_guild_data(test_guild_id, verifiedRoleName=test_input)
 
         assert (
-            await fetch_guild_data(1, "verifiedRoleName")
+            await fetch_guild_data(test_guild_id, "verifiedRoleName")
         ).verifiedRoleName == test_input
 
     @pytest.mark.parametrize("test_input", [5, 0, -1])
     @pytest.mark.asyncio
     async def test_update_guild_data_invalid_input_fails(
-        self, test_input, start_docker_services, wait_for_redis
+        self, test_input: int, test_guild_id: int
     ):
         with pytest.raises(ValidationError) as e:
-            await update_guild_data(1, verifiedRoleName=test_input)
+            await update_guild_data(test_guild_id, verifiedRoleName=test_input)
 
         assert issubclass(e.type, ValidationError)
