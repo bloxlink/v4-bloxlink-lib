559421a4019274dccd9529561fd0e7f15d9cb09f
Julien Kmec <me@julien.dev>
2025-05-14 18:37:31 -0700

    add additional test

--- tests/unit/test_nicknames.py
+++ tests/unit/test_nicknames.py
@@ -94,6 +94,12 @@ class TestNicknames:
                 include_discord_user=True,
                 include_roblox_user=False,
             ),
+            NicknameTestData(
+                nickname_template="{roblox-name}",
+                expected_nickname="{roblox_user.username}",
+                include_discord_user=True,
+                include_roblox_user=False,
+            ),
         ],
     )
     async def test_shorter_nicknames(
@@ -105,6 +111,62 @@ class TestNicknames:
         mocker: MockerFixture,
     ):
         """Test that templates with brackets are parsed correctly if shorter nicknames are enabled."""
+
+        expected_nickname = test_case.expected_nickname
+        nickname_template = test_case.nickname_template
+
+        expected_nickname = nickname_formatter(
+            expected_nickname_format=expected_nickname,
+            roblox_user=test_group_member_bracketed_roleset.roblox_user,
+            discord_user=test_group_member_bracketed_roleset.discord_user,
+            guild=test_guild,
+        )
+
+        group_bind = mock_bind(
+            mocker,
+            discord_roles=[],
+            criteria=BindCriteria(
+                type="group", id=test_group.id, group=GroupBindData(everyone=True)
+            ),
+            entity=test_group,
+        )
+
+        nickname = await parse_template(
+            guild_id=test_guild.id,
+            guild_name=test_guild.name,
+            member=test_group_member_bracketed_roleset.discord_user,
+            template=nickname_template,
+            potential_binds=[group_bind],
+            roblox_user=test_group_member_bracketed_roleset.roblox_user,
+            trim_nickname=False,  # Parse the entire template
+            shorter_nicknames=True,
+        )
+
+        assert (
+            nickname == expected_nickname
+        ), f"Expected nickname to be {expected_nickname}, got {nickname}"
+
+    @pytest.mark.asyncio_concurrent(group="nickname_generic_tests")
+    @pytest.mark.parametrize(
+        "test_case",
+        [
+            NicknameTestData(
+                nickname_template="{roblox-name}",
+                expected_nickname="{roblox_user.username}",
+                include_discord_user=True,
+                include_roblox_user=False,
+            ),
+        ],
+    )
+    async def test_shorter_nicknames_disabled(
+        self,
+        test_guild: "GuildSerializable",
+        test_group: RobloxGroup,
+        test_group_member_bracketed_roleset: MockUser,
+        test_case: NicknameTestData,
+        mocker: MockerFixture,
+    ):
+        """Test that templates with brackets are parsed with full roleset names if shorter nicknames are disabled."""
 
         expected_nickname = test_case.expected_nickname
         nickname_template = test_case.nickname_template
