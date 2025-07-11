0366a5817bc79b696e0db264f78ca9b52197a1e4
Julien Kmec <me@julien.dev>
2025-07-11 14:53:47 -0700

    metrics in thread

--- bloxlink_lib/metrics.py
+++ bloxlink_lib/metrics.py
@@ -1,10 +1,12 @@
 import logging
+import contextlib
+import time
+import threading
 import uvicorn
-from bloxlink_lib.utils import create_task_log_exception
-from .config import CONFIG
 from fastapi import APIRouter, FastAPI
 from starlette.responses import PlainTextResponse
 from prometheus_client import generate_latest
+from .config import CONFIG
 
 router = APIRouter(tags=["Metrics"])
 
@@ -16,21 +18,36 @@ async def metrics():
     return PlainTextResponse(generate_latest())
 
 
-async def main():
-    """Starts the metrics server."""
+class MetricsServer(uvicorn.Server):
+    """
+    A custom server that runs in a separate thread for metrics.
+    """
 
+    @contextlib.contextmanager
+    def run_in_thread(self):
+        thread = threading.Thread(target=self.run)
+        thread.start()
+        try:
+            while not self.started:
+                time.sleep(1e-3)
+            yield
+        finally:
+            self.should_exit = True
+            thread.join()
+
+
+if __name__ == "__main__":
     if not CONFIG.METRICS_ENABLED:
         logging.info(
             "Metrics are disabled. To enable metrics, set METRICS_ENABLED to True."
         )
-        return
+
+    app = FastAPI()
+    app.include_router(router)
 
     logging.info(
         f"Starting metrics server on {CONFIG.METRICS_HOST}:{CONFIG.METRICS_PORT}"
     )
-
-    app = FastAPI()
-    app.include_router(router)
 
     config = uvicorn.Config(
         app,
@@ -38,8 +55,9 @@ async def main():
         port=int(CONFIG.METRICS_PORT),
         log_level=CONFIG.LOG_LEVEL.lower(),
     )
-    server = uvicorn.Server(config)
-    await server.serve()
+    server = MetricsServer(config=config)
 
-
-create_task_log_exception(main())
+    with server.run_in_thread():
+        logging.info("Metrics server started")
+        while True:
+            time.sleep(1)
