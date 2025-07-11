260e19618c2a489049a46a2be427f33f8449051d
Julien Kmec <me@julien.dev>
2025-07-11 15:13:44 -0700

    unused

--- bloxlink_lib/config.py
+++ bloxlink_lib/config.py
@@ -14,7 +14,6 @@ class BaseConfig(BaseSettings):
     #############################
     LOG_LEVEL: Literal["DEBUG", "INFO", "WARNING", "ERROR"] = "INFO"
     NODE_LOCK_TTL: int = 600
-    APP_NAME: str | None
     #############################
     # METRICS AND LOGGING
     #############################
